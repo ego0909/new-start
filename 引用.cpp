5fe0814fd59ca9028fcd4550ddfac5f10c5da2d9
ego0909 <71870917+ego0909@users.noreply.github.com>
2020-09-27 10:47:52 +0800

    Add files via upload

---
+++ 引用.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+class Student {
+public:
+	//属性和行为"
+	int number;//学号
+	string name;//姓名
+
+	void showstudent() {
+		cout << "姓名" << name << "     " << "学号" << number << endl;
+	}
+	void setname(string s) {
+		name = s;
+	}
+};
+
+	
+int main() {
+	//创建一个具体的学生
+	Student s1;
+	s1.name = "张三";
+		s1.number = 21;
+		
+		s1.number = 32;
+		s1.showstudent();
+
+		Student c1;
+		c1.name = "肖轶戈";
+		c1.number = 33;
+		c1.showstudent();
+	system("pause");
+	return 0;
+}
